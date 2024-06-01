8adeade8d6978afda26ae246721702df0c3322e3
王震 <wangzhen350@huawei.com>
2024-06-01 18:25:38 +0800

    ipc通信接口校验
    Signed-off-by: 王震 <wangzhen350@huawei.com>

--- services/server/src/socperf_stub.cpp
+++ services/server/src/socperf_stub.cpp
@@ -25,6 +25,9 @@ namespace OHOS {
 namespace SOCPERF {
 namespace {
     constexpr int32_t HIVIEW_UID = 1201;
+    constexpr int32_t MSG_STRING_MAX_LEN = 1024;
+    constexpr int32_t MSG_VECTOR_MAX_LEN = 1024;
+    constexpr int32_t MSG_VECTOR_INVALID_LEN = 0;
     const int32_t ENG_MODE = OHOS::system::GetIntParameter("const.debuggable", 0);
 }
 int32_t SocPerfStub::OnRemoteRequest(uint32_t code, MessageParcel &data,
@@ -36,38 +39,23 @@ int32_t SocPerfStub::OnRemoteRequest(uint32_t code, MessageParcel &data,
     }
     switch (code) {
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_PERF_REQUEST): {
-            int32_t cmdId = data.ReadInt32();
-            std::string msg = data.ReadString();
-            PerfRequest(cmdId, msg);
+            StubPerfRequest(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_PERF_REQUEST_EX): {
-            int32_t cmdId = data.ReadInt32();
-            bool onOffTag = data.ReadBool();
-            std::string msg = data.ReadString();
-            PerfRequestEx(cmdId, onOffTag, msg);
+            StubPerfRequestEx(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_POWER_LIMIT_BOOST_FREQ): {
-            bool onOffTag = data.ReadBool();
-            std::string msg = data.ReadString();
-            PowerLimitBoost(onOffTag, msg);
+            StubPowerLimitBoost(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_THERMAL_LIMIT_BOOST_FREQ): {
-            bool onOffTag = data.ReadBool();
-            std::string msg = data.ReadString();
-            ThermalLimitBoost(onOffTag, msg);
+            StubThermalLimitBoost(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_LIMIT_REQUEST): {
-            int32_t clientId = data.ReadInt32();
-            std::vector<int32_t> tags;
-            data.ReadInt32Vector(&tags);
-            std::vector<int64_t> configs;
-            data.ReadInt64Vector(&configs);
-            std::string msg = data.ReadString();
-            LimitRequest(clientId, tags, configs, msg);
+            StubLimitRequest(data);
             break;
         }
         default:
@@ -81,36 +69,167 @@ int32_t SocPerfStub::OnRemoteRequestExt(uint32_t code, MessageParcel &data,
 {
     switch (code) {
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_SET_STATUS): {
-            bool requestEnable = data.ReadBool();
-            std::string msg = data.ReadString();
-            SetRequestStatus(requestEnable, msg);
+            StubSetRequestStatus(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_SET_THERMAL_LEVEL): {
-            SetThermalLevel(data.ReadInt32());
+            StubSetThermalLevel(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_SET_DEVICE_MODE): {
-            std::string mode = data.ReadString();
-            bool status = data.ReadBool();
-            RequestDeviceMode(mode, status);
+            StubRequestDeviceMode(data);
             break;
         }
         case static_cast<uint32_t>(SocPerfInterfaceCode::TRANS_IPC_ID_REQUEST_CMDID_COUNT): {
-            int32_t callingUid = IPCSkeleton::GetCallingUid();
-            std::string msg;
-            if ((ENG_MODE == 0 && callingUid != HIVIEW_UID) || !data.ReadString(msg)) {
-                SOC_PERF_LOGE("not have right to do RequestCmdIdCount");
-                return ERR_INVALID_STATE;
-            }
-            if (!reply.WriteString(RequestCmdIdCount(msg))) {
-                SOC_PERF_LOGE("write RequestCmdIdCount ret failed");
-                return ERR_INVALID_STATE;
-            }
+            StubRequestDeviceMode(data, reply);
             break;
         }
         default:
             return IPCObjectStub::OnRemoteRequest(code, data, reply, option);
+    }
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubPerfRequest(MessageParcel &data)
+{
+    int32_t cmdId = 0;
+    READ_PARCEL(data, Int32, cmdId, ERR_INVALID_STATE, SocPerfStub);
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    PerfRequest(cmdId, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubPerfRequestEx(MessageParcel &data)
+{
+    int32_t cmdId = 0;
+    READ_PARCEL(data, Int32, cmdId, ERR_INVALID_STATE, SocPerfStub);
+
+    bool onOffTag = false;
+    READ_PARCEL(data, Bool, onOffTag, ERR_INVALID_STATE, SocPerfStub);
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    PerfRequestEx(cmdId, onOffTag, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubPowerLimitBoost(MessageParcel &data)
+{
+    bool onOffTag = false;
+    READ_PARCEL(data, Bool, onOffTag, ERR_INVALID_STATE, SocPerfStub);
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    PowerLimitBoost(onOffTag, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubThermalLimitBoost(MessageParcel &data)
+{
+    bool onOffTag = false;
+    READ_PARCEL(data, Bool, onOffTag, ERR_INVALID_STATE, SocPerfStub);
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    ThermalLimitBoost(onOffTag, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubLimitRequest(MessageParcel &data)
+{
+    int32_t clientpid;
+    READ_PARCEL(data, Int32, clientId, ERR_INVALID_STATE, SocPerfStub);
+
+    std::vector<int32_t> tags;
+    READ_PARCEL(data, Int32Vector, &tags, ERR_INVALID_STATE, SocPerfStub);
+    if (tags.size() <= MSG_VECTOR_INVALID_LEN || tags.size() > MSG_VECTOR_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    std::vector<int64_t> configs;
+    READ_PARCEL(data, Int32Vector, &configs, ERR_INVALID_STATE, SocPerfStub);
+    if (configs.size() <= MSG_VECTOR_INVALID_LEN || configs.size() > MSG_VECTOR_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    LimitRequest(clientId, tags, configs, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubSetRequestStatus(MessageParcel &data)
+{
+    bool requestEnable;
+    READ_PARCEL(data, Bool, requestEnable, ERR_INVALID_STATE, SocPerfStub);
+
+    std::string msg;
+    READ_PARCEL(data, String, msg, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    SetRequestStatus(requestEnable, msg);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubSetThermalLevel(MessageParcel &data)
+{
+    int32_t levelId;
+    READ_PARCEL(data, Int32, levelId, ERR_INVALID_STATE, SocPerfStub);
+
+    SetThermalLevel(levelId);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubRequestDeviceMode(MessageParcel &data)
+{
+    std::string mode;
+    READ_PARCEL(data, String, mode, ERR_INVALID_STATE, SocPerfStub);
+    if (msg.length() > MSG_STRING_MAX_LEN) {
+        return ERR_INVALID_STATE;
+    }
+
+    bool status;
+    READ_PARCEL(data, Bool, status, ERR_INVALID_STATE, SocPerfStub);
+
+    RequestDeviceMode(mode, status);
+    return ERR_OK;
+}
+
+int32_t SocPerfStub::StubRequestDeviceMode(MessageParcel &data, MessageParcel &reply)
+{
+    int32_t callingUid = IPCSkeleton::GetCallingUid();
+    std::string msg;
+    if ((ENG_MODE == 0 && callingUid != HIVIEW_UID) || !data.ReadString(msg)) {
+        SOC_PERF_LOGE("not have right to do RequestCmdIdCount");
+        return ERR_INVALID_STATE;
+    }
+    if (!reply.WriteString(RequestCmdIdCount(msg))) {
+        SOC_PERF_LOGE("write RequestCmdIdCount ret failed");
+        return ERR_INVALID_STATE;
     }
     return ERR_OK;
 }
