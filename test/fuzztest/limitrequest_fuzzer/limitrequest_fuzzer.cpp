dcf09e4b0bbfe9698e7ec7b6c3cc223a53e1c4f2
王震 <wangzhen350@huawei.com>
2024-08-05 22:00:17 +0800

    FUZZ添加权限
    Signed-off-by: 王震 <wangzhen350@huawei.com>

--- test/fuzztest/limitrequest_fuzzer/limitrequest_fuzzer.cpp
+++ test/fuzztest/limitrequest_fuzzer/limitrequest_fuzzer.cpp
@@ -37,7 +37,7 @@ namespace SOCPERF {
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
-    MockProcess()
+    OHOS::SOCPERF::MockProcess();
     OHOS::SOCPERF::TestLimitRequest(data, size);
     return 0;
 }
