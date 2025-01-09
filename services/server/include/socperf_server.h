f7c2827b88cf2996da64f117efca40eab0d956ee
0error <zhaogang52@huawei.com>
2025-01-09 15:33:24 +0800

    add IDL in socperf
    Signed-off-by: 0error <zhaogang52@huawei.com>

--- services/server/include/socperf_server.h
+++ services/server/include/socperf_server.h
@@ -17,7 +17,7 @@
 #define SOC_PERF_SERVICES_SERVER_INCLUDE_SOCPERF_SERVER_H
 
 #include "singleton.h"
-#include "socperf_stub.h"
+#include "soc_perf_stub.h"
 #include "socperf.h"
 #include "system_ability.h"
 
@@ -30,16 +30,79 @@ DECLARE_SYSTEM_ABILITY(SocPerfServer);
 DECLARE_DELAYED_SINGLETON(SocPerfServer);
 
 public:
-    void PerfRequest(int32_t cmdId, const std::string& msg) override;
-    void PerfRequestEx(int32_t cmdId, bool onOffTag, const std::string& msg) override;
-    void PowerLimitBoost(bool onOffTag, const std::string& msg) override;
-    void ThermalLimitBoost(bool onOffTag, const std::string& msg) override;
-    void LimitRequest(int32_t clientId,
+    /**
+     * @brief Sending a performance request.
+     *
+     * @param cmdId Scene id defined in config file.
+     * @param msg Additional string info, which is used for other extensions.
+     */
+    virtual ErrCode PerfRequest(int32_t cmdId, const std::string& msg) override;
+
+    /**
+     * @brief Sending a performance request.
+     *
+     * @param cmdId Scene id defined in config file.
+     * @param onOffTag Indicates the start of end of a long-term frequency increase event.
+     * @param msg Additional string info, which is used for other extensions.
+     */
+    virtual ErrCode PerfRequestEx(int32_t cmdId, bool onOffTag, const std::string& msg) override;
+
+    /**
+     * @brief Sending a power limit boost request.
+     *
+     * @param onOffTag Indicates the start of end of a power limit boost event.
+     * @param msg Additional string info, which is used for other extensions.
+     */
+    virtual ErrCode PowerLimitBoost(bool onOffTag, const std::string& msg) override;
+
+    /**
+     * @brief Sending a thermal limit boost request.
+     *
+     * @param onOffTag Indicates the start of end of a thermal limit boost event.
+     * @param msg Additional string info, which is used for other extensions.
+     */
+    virtual ErrCode ThermalLimitBoost(bool onOffTag, const std::string& msg) override;
+
+    /**
+     * @brief Sending a limit request.
+     *
+     * @param clientId Used to indentify the caller of frequency limiting, such as
+     * the thermal module or power consumption module.
+     * @param configs Indicates the specific value to be limited.
+     * @param msg Additional string info, which is used for other extensions.
+     */
+    virtual ErrCode LimitRequest(int32_t clientId,
         const std::vector<int32_t>& tags, const std::vector<int64_t>& configs, const std::string& msg) override;
-    void SetRequestStatus(bool status, const std::string& msg) override;
-    void SetThermalLevel(int32_t level) override;
-    void RequestDeviceMode(const std::string& mode, bool status) override;
-    std::string RequestCmdIdCount(const std::string& msg) override;
+
+    /**
+     * @brief set socperf server status, enable or disable
+     *
+     * @param status true means enable socperfserver, false means disable socperfserver
+     * @param msg the reason why we need change socperfserver status
+     */
+    virtual ErrCode SetRequestStatus(bool status, const std::string& msg) override;
+
+    /**
+     * @brief set thermal level intermal for perfquest
+     *
+     * @param level thermal level
+     */
+    virtual ErrCode SetThermalLevel(int32_t level) override;
+
+    /**
+     * @brief send the device mode, enable or disable
+     *
+     * @param mode the mode which will to be changed
+     * @param status true means socperfserver enter the device mode, false quit the device mode
+     */
+    virtual ErrCode RequestDeviceMode(const std::string& mode, bool status) override;
+
+    /**
+     * @brief get cmd Id count, cmdID is trigger, its count++
+     * @param msg the reason
+     * @param funcResult return cmdId count, as 10000:xx,10001:xx
+     */
+    virtual ErrCode RequestCmdIdCount(const std::string& msg, const std::string& funcResult) override;
     int32_t Dump(int32_t fd, const std::vector<std::u16string>& args) override;
 
 public:
@@ -52,6 +115,7 @@ protected:
 private:
     SocPerf socPerf;
     bool AllowDump();
+    bool HasPerfPermission();
 };
 } // namespace SOCPERF
 } // namespace OHOS
