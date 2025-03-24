be9e600fedafbad34c6f300a0ddf5af204e91d42
wangz <wangzhen350@huawei.com>
2025-03-24 22:12:39 +0800

    socperf功耗优化
    Signed-off-by: wangz <wangzhen350@huawei.com>

--- services/core/include/socperf.h
+++ services/core/include/socperf.h
@@ -48,7 +48,6 @@ private:
         std::vector<std::unordered_map<int32_t, int32_t>>(ACTION_TYPE_MAX);
     volatile bool perfRequestEnable_ = true;
     int32_t thermalLvl_ = INVALID_THERMAL_LVL;
-    int32_t minThermalLvl_ = INVALID_THERMAL_LVL;
     SocPerfConfig &socPerfConfig_ = SocPerfConfig::GetInstance();
     std::unordered_map<int32_t, uint32_t> boostCmdCount_;
     std::unordered_map<int32_t, uint64_t> boostTime_;
